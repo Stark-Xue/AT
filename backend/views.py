07dfb00935f81ea113787a61c9d4bada6b3e1aa4
Stark-Xue <1595627859@qq.com>
2021-01-04 09:59:50 +0800

    @xframe_options_sameorigin加到upload_img

--- backend/views.py
+++ backend/views.py
@@ -331,7 +331,7 @@ def upload_companyImg(request):
 
 
 @check_login
-@xframe_options_exempt
+@xframe_options_sameorigin
 def upload_img(request):
     #print(request.FILES["imgFile"], type(request.FILES))
     img_obj = request.FILES.get('imgFile')
